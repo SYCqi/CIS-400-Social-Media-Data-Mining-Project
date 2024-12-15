7e71c758a1419c5b1b05b642e6a97eb474eedcca
Sonya Yim <91685133+SYCqi@users.noreply.github.com>
2024-12-15 15:36:23 -0500

    Update makeVisualization2.py

--- Word Cloud Vizualization/makeVisualization2.py
+++ Word Cloud Vizualization/makeVisualization2.py
@@ -1,9 +1,13 @@
+# Creating word clouds for the preprocessed data for vizualization
+#
 import pandas as pd
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
 # Load the data
-file_path = r"C:\Users\mlors\PycharmProjects\cis400Project\cleanData\clean_temperate_region_sunnyData.csv"
+# Remember to change the file_path to yours where you have stored the preprocessed data of the format clean_(region)_region_(weather)Data.csv
+#
+file_path = r"C:\Users\mlors\PycharmProjects\cis400Project\cleanData\clean_temperate_region_sunnyData.csv" 
 data = pd.read_csv(file_path)
 
 # Extract the third keyword from the 'KEYWORD' column
